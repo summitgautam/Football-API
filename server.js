f9938e7789e5e5e041edc0a6b8ccfe17853b01bb
summitgautam <summitbro1@gmail.com>
2026-06-14 18:22:53 +0545

    currently making routes, already made a lot of them like top dribblers, passers and more now ig the api will be done quickly i just need to make the index.html afterwards

--- server.js
+++ server.js
@@ -23,8 +23,96 @@ fs.readFile("./football_players_data.json", "utf8",(err,data)=>{
 });
 
 app.get('/',(req,res)=>{
-    
+    res.json({
+        name: "Football Players API",
+        description: "This is an API that provides the info about many football players.",
+        version: "1.0.1",
+        endpoints:[
+            "/api/players - GET all players",
+            "api/players/:name - Get player by name",
+            "/api/top/:n - Get top n players",
+            "/api/country/:country - Get players by country",
+            "/api/position/:position - Get players by specific position",
+            "/api/rating/:rating - Get players with rating specified",
+            "/api/random - Get random player",
+            "/api/nationality-list - Get list of nationalities",
+            "/api/top-dribblers - Get top 10 dribblers",
+            "/api/top-passers - Get top 10 passers",
+            "/api/top-pacers - Get top 10 pacers",
+            "/api/top-finishers - Get top 10 finishers",
+            "/api/best-value - Get most valuable players"
+        ]
+    })
 });
+app.get("/api/players",(req,res)=>{
+    const limit = parseInt(req.query.limit);
+    if(limit){
+        return res.json(players.slice(0,limit));
+    }
+    res.json(players);
+});
+
+app.get("/api/player/:name", (req,res)=>{
+    const name = req.params.name.toLowerCase();
+    const result = players.filter(player=> player.full_name.toLowerCase().includes(name));
+    res.json(result);
+});
+
+app.get("/api/top/:n", (req,res)=>{
+    const n = parseInt(req.params.n);
+    const result= [...players].sort((a,b)=> b.overall_rating - a.overall_rating).slice(0,n);
+    res.json(result);
+})
+
+app.get("/api/country/:country", (req,res)=>{
+    const country = req.params.country.toLowerCase();
+    const result = players.filter(p=> p.nationality.toLowerCase()===country);
+    res.json(result);
+});
+
+app.get("/api/position/:position",(req,res)=>{
+    const position = req.params.position.toLowerCase();
+    const result = players.filter(p=>p.positions.includes(position));
+    res.json(result);
+})
+
+app.get("/api/rating/:rating",(req,res)=>{
+    const rating = parseInt(req.params.rating);
+    const result = players.filter(p=>p.overall_rating = rating);
+    res.json(result);
+})
+
+app.get("/api/random", (req,res)=>{
+    const randomplayer = players[Math.floor(Math.random()*players.length)];
+    res.json(randomplayer);
+})
+
+app.get("/api/best-value",(req,res)=>{
+    const result = [...players].sort((a,b)=> 
+    (b.overall_rating/b.market_value)= (a.overall_rating/a.market_value)).slice(0,10);
+    res.json(result);
+})
+
+app.get("/api/nationality-list",(req,res)=>{
+    const countries = [...new Set(players.map(p=>p.nationality))].sort();
+    res.json(countries);
+})
+
+app.get("/api/top-dribblers",(req,res)=>{
+    const result = [...players].sort((a,b)=>b.dribbling-a.dribbling).slice(0,10);
+    res.json(result);
+})
+
+app.get("/api/top-finishers",(req,res)=>{
+    const result = [...players].sort((a,b)=>b.finishing-a.finishing).slice(0,10);
+    res.json(result);
+})
+
+app.get("/api/top-passers",(req,res)=>{
+    const result = [...players].sort((a,b)=> b.passing-a.passing).slice(0,10);
+    res.json(result);
+})
+
 
 app.listen(port,()=>{
     console.log(`Visit http://localhost:${port}`);
